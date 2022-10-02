d466617c5079ea5b039f00aafe6a2c3b04ae617a
DM-BSNU2022 <dmbsnu2022@gmail.com>
2022-10-02 12:24:13 +0300

    Final template

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,13 +2,6 @@
 	
 	"use strict";
 
-	// Page loading animation
-	$(window).on('load', function() {
-
-        $('#js-preloader').addClass('loaded');
-
-    });
-
 	// WOW JS
 	$(window).on ('load', function (){
         if ($(".wow").length) { 
@@ -46,24 +39,177 @@
         });
 
         var $grid = $(".grid").isotope({
-          	itemSelector: ".all",
-          	percentPosition: true,
-          	masonry: {
+          itemSelector: ".all",
+          percentPosition: true,
+          masonry: {
             columnWidth: ".all"
-        }
-    })
-
-	var width = $(window).width();
-		$(window).resize(function() {
-			if (width > 992 && $(window).width() < 992) {
-				location.reload();
-			}
-			else if (width < 992 && $(window).width() > 992) {
-				location.reload();
-			}
-	})
+          }
+        })
 
 
+	const Accordion = {
+	  settings: {
+	    // Expand the first item by default
+	    first_expanded: false,
+	    // Allow items to be toggled independently
+	    toggle: false
+	  },
+
+	  openAccordion: function(toggle, content) {
+	    if (content.children.length) {
+	      toggle.classList.add("is-open");
+	      let final_height = Math.floor(content.children[0].offsetHeight);
+	      content.style.height = final_height + "px";
+	    }
+	  },
+
+	  closeAccordion: function(toggle, content) {
+	    toggle.classList.remove("is-open");
+	    content.style.height = 0;
+	  },
+
+	  init: function(el) {
+	    const _this = this;
+
+	    // Override default settings with classes
+	    let is_first_expanded = _this.settings.first_expanded;
+	    if (el.classList.contains("is-first-expanded")) is_first_expanded = true;
+	    let is_toggle = _this.settings.toggle;
+	    if (el.classList.contains("is-toggle")) is_toggle = true;
+
+	    // Loop through the accordion's sections and set up the click behavior
+	    const sections = el.getElementsByClassName("accordion");
+	    const all_toggles = el.getElementsByClassName("accordion-head");
+	    const all_contents = el.getElementsByClassName("accordion-body");
+	    for (let i = 0; i < sections.length; i++) {
+	      const section = sections[i];
+	      const toggle = all_toggles[i];
+	      const content = all_contents[i];
+
+	      // Click behavior
+	      toggle.addEventListener("click", function(e) {
+	        if (!is_toggle) {
+	          // Hide all content areas first
+	          for (let a = 0; a < all_contents.length; a++) {
+	            _this.closeAccordion(all_toggles[a], all_contents[a]);
+	          }
+
+	          // Expand the clicked item
+	          _this.openAccordion(toggle, content);
+	        } else {
+	          // Toggle the clicked item
+	          if (toggle.classList.contains("is-open")) {
+	            _this.closeAccordion(toggle, content);
+	          } else {
+	            _this.openAccordion(toggle, content);
+	          }
+	        }
+	      });
+
+	      // Expand the first item
+	      if (i === 0 && is_first_expanded) {
+	        _this.openAccordion(toggle, content);
+	      }
+	    }
+	  }
+	};
+
+	(function() {
+	  // Initiate all instances on the page
+	  const accordions = document.getElementsByClassName("accordions");
+	  for (let i = 0; i < accordions.length; i++) {
+	    Accordion.init(accordions[i]);
+	  }
+	})();
+	
+
+	(function init() {
+		function getTimeRemaining(endtime) {
+		  var t = Date.parse(endtime) - Date.parse(new Date());
+		  var seconds = Math.floor((t / 1000) % 60);
+		  var minutes = Math.floor((t / 1000 / 60) % 60);
+		  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+		  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+		  return {
+			'total': t,
+			'days': days,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		  };
+		}
+		
+		function initializeClock(endtime){
+		var timeinterval = setInterval(function(){
+		  var t = getTimeRemaining(endtime);
+		  document.querySelector(".days > .value").innerText=t.days;
+		  document.querySelector(".hours > .value").innerText=t.hours;
+		  document.querySelector(".minutes > .value").innerText=t.minutes;
+		  document.querySelector(".seconds > .value").innerText=t.seconds;
+		  if(t.total<=0){
+			clearInterval(timeinterval);
+		  }
+		},1000);
+	  }
+	  initializeClock((new Date('2022-10-06T00:00:00')) )
+	})()
+
+	var context;
+	var $window = $(window);
+
+	// run this right away to set context
+	if ($window.width() <= 768) {
+		context = 'small';
+	} else if (768 < $window.width() < 970) {
+		context = 'medium';
+	} else {
+		context = 'large';
+	}
+
+	// refresh the page only if you're crossing into a context
+	// that isn't already set
+	$(window).resize(function() {
+		if(($window.width() <= 768) && (context != 'small')) {
+			//refresh the page
+			location.reload();
+		} else if ((768 < $window.width()  < 970) && (context != 'medium')) {
+			location.reload();
+		} else if (context != 'large') {
+			location.reload();
+		}
+	});
+
+	$("#modal_trigger").leanModal({
+		top: 100,
+		overlay: 0.6,
+		closeButton: ".modal_close"
+	});
+
+	$(function() {
+		// Calling Login Form
+		$("#login_form").click(function() {
+			$(".social_login").hide();
+			$(".user_login").show();
+			return false;
+		});
+
+		// Calling Register Form
+		$("#register_form").click(function() {
+			$(".social_login").hide();
+			$(".user_register").show();
+			$(".header_title").text('Register');
+			return false;
+		});
+
+		// Going back to Social Forms
+		$(".back_btn").click(function() {
+			$(".user_login").hide();
+			$(".user_register").hide();
+			$(".social_login").show();
+			$(".header_title").text('Login');
+			return false;
+		});
+	});
 
 	$(document).on("click", ".naccs .menu div", function() {
 		var numberIndex = $(this).index();
@@ -82,37 +228,34 @@
 		}
 	});
 
-	$('.owl-features').owlCarousel({
-		items:3,
+	$('.owl-testimonials').owlCarousel({
+		items:1,
 		loop:true,
-		dots: false,
-		nav: true,
+		dots: true,
+		nav: false,
 		autoplay: true,
 		margin:30,
-		responsive:{
+		  responsive:{
 			  0:{
 				  items:1
 			  },
 			  600:{
-				  items:2
+				  items:1
 			  },
-			  1200:{
-				  items:3
-			  },
-			  1800:{
-				items:3
-			}
-		}
-	})
+			  1000:{
+				  items:1
+			  }
+		  }
+	  })
 
-	$('.owl-collection').owlCarousel({
+	$('.owl-features').owlCarousel({
 		items:3,
 		loop:true,
-		dots: false,
+		dots: true,
 		nav: true,
 		autoplay: true,
 		margin:30,
-		responsive:{
+		  responsive:{
 			  0:{
 				  items:1
 			  },
@@ -121,31 +264,9 @@
 			  },
 			  1000:{
 				  items:3
-			}
-		}
-	})
-
-	$('.owl-banner').owlCarousel({
-		items:1,
-		loop:true,
-		dots: false,
-		nav: true,
-		autoplay: true,
-		margin:30,
-		responsive:{
-			  0:{
-				  items:1
-			  },
-			  600:{
-				  items:1
-			  },
-			  1000:{
-				  items:1
-			}
-		}
-	})
-
-	
+			  }
+		  }
+	  })
 	
 	
 
@@ -276,7 +397,42 @@
     }
 
 
-	
+	function visible(partial) {
+        var $t = partial,
+            $w = jQuery(window),
+            viewTop = $w.scrollTop(),
+            viewBottom = viewTop + $w.height(),
+            _top = $t.offset().top,
+            _bottom = _top + $t.height(),
+            compareTop = partial === true ? _bottom : _top,
+            compareBottom = partial === true ? _top : _bottom;
+
+        return ((compareBottom <= viewBottom) && (compareTop >= viewTop) && $t.is(':visible'));
+
+    }
+
+    $(window).scroll(function() {
+
+        if (visible($('.count-digit'))) {
+            if ($('.count-digit').hasClass('counter-loaded')) return;
+            $('.count-digit').addClass('counter-loaded');
+
+            $('.count-digit').each(function() {
+                var $this = $(this);
+                jQuery({
+                    Counter: 0
+                }).animate({
+                    Counter: $this.text()
+                }, {
+                    duration: 3000,
+                    easing: 'swing',
+                    step: function() {
+                        $this.text(Math.ceil(this.Counter));
+                    }
+                });
+            });
+        }
+    })
 
 
 })(window.jQuery);
